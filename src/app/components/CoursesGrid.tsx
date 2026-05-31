cd67ef7b35261662eefe217d01744a972ff0231c
alishabbanni-hub <ali.shabbanni@gmail.com>
2026-05-31 14:32:44 -0400

    Update CoursesGrid.tsx

--- src/app/components/CoursesGrid.tsx
+++ src/app/components/CoursesGrid.tsx
@@ -24,8 +24,8 @@ export function CoursesGrid({ category, onCourseSelect }: CoursesGridProps) {
     },
     {
       id: 2,
-      title: 'Fostering Deep Engagement',
-      subtitle: "Project-Based Learning in Action",
+      title: 'Project-Based Learning in Action',
+      subtitle: "Fostering Deep Engagement",
       instructor: 'Michael Chen',
       image: 'https://plus.unsplash.com/premium_photo-1664910131883-132940940995?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       duration: '4 weeks',
